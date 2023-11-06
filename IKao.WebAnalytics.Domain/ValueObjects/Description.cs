945663a5346a973119068611aea3f5a6f87ddf98
Aleksei Kostrykin <me@ikao.dev>
2023-11-06 18:43:58 +0300

    feat: some ValueObjects for Game DTO

---
+++ IKao.WebAnalytics.Domain/ValueObjects/Description.cs
@@ -0,0 +1,19 @@
+﻿namespace IKao.WebAnalytics.Domain.ValueObjects;
+
+public class Description : ValueObject
+{
+    public string Value { get; private set; }
+
+    public Description(string value)
+    {
+        if (IsLengthValid(value) == false)
+            throw new ArgumentException(nameof(IsLengthValid));
+        
+        Value = value;
+    }
+
+    private bool IsLengthValid(string text)
+    {
+        return text.Length < 255;
+    }
+}
---
+++ IKao.WebAnalytics.Domain/ValueObjects/Name.cs
@@ -0,0 +1,19 @@
+﻿namespace IKao.WebAnalytics.Domain.ValueObjects;
+
+public class Name : ValueObject
+{
+    public string Value { get; private set; }
+
+    public Name(string value)
+    {
+        if (IsLengthValid(value) == false)
+            throw new ArgumentException(nameof(IsLengthValid));
+        
+        Value = value;
+    }
+
+    private bool IsLengthValid(string text)
+    {
+        return text.Length < 255;
+    }
+}
