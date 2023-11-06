1b0b6a23c12860271ac7cf148610c9d64ea11624
Aleksei Kostrykin <me@ikao.dev>
2023-11-06 19:32:56 +0300

    feat: created game entity model for EF Core

--- IKao.WebAnalytics.Domain/Model/Game.cs
+++ IKao.WebAnalytics.Domain/Model/Game.cs
@@ -1,6 +1,27 @@
-﻿namespace IKao.WebAnalytics.Domain.Model;
+﻿using IKao.WebAnalytics.Domain.DTO;
+using IKao.WebAnalytics.Domain.ValueObjects;
+using NodaTime;
 
-public class Game
+namespace IKao.WebAnalytics.Domain.Model;
+
+public class Game : Entity<AppId>
 {
+    private Name _title;
+    private Description _description;
+    private Description _seo;
+    private Description _instruction;
+    private Developer _developer;
+
+    private AgeRating _age;
+    private Rating _rating;
+    private Counter _players;
+
+    private Media _media;
     
+    private Url _play;
+
+    private Instant? _published;
+    private Instant? _created;
+    private Instant? _updated;
+    private Instant? _removed;
 }
