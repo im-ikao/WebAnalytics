4f08ce22d68a12db239da8ccae180267ba8856e0
Aleksei Kostrykin <me@ikao.dev>
2023-11-06 20:36:55 +0300

    feat: base Entity Framework Core Repository

---
+++ IKao.WebAnalytics.Infrastructure/BaseEFRepository.cs
@@ -0,0 +1,26 @@
+﻿using IKao.WebAnalytics.Domain.Abstraction;
+
+namespace IKao.WebAnalytics.Infrastructure;
+
+public class BaseEFRepository : IRepository
+{
+    public TEntity InsertOrUpdate<TEntity>(TEntity entity) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> InsertOrUpdateAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InsertOrUpdateBulk<TEntity>(TEntity[] entity) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertOrUpdateBulkAsync<TEntity>(TEntity[] entity, CancellationToken cancellationToken = default) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+}
